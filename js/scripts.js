e71135e26ac4d22a49bf5aca68384074c9d77124
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-26 14:01:28 -0700

    add else if statments to js page, debug js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
- 
+$(document).ready(function() {
+  $("form#measurements").submit(function(event) {
+    var side1 = $("input#side1").val();
+    var side2 = $("input#side2").val();
+    var side3 = $("input#side3").val();
+  }
+
+  if (side1 === side2 && side2 === side3) {
+    $('#equilateral').show();
+  
+  } else if {
+    (side1 === side2 ! side3)
+    $('#isosceles').show();
+
+  } else if {
+    (side1 ! side2 ! side3)
+    $('#scalene').show();
+
+  } else {
+    (side1 + side2 <= side3)
+    $('#noTriangle').show();
+
+  }
+  event.preventDefault();
+
+}
