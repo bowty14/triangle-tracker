e660491aa4b4554655cd056a1fe3a106c535932b
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-26 16:00:44 -0700

    add blood sweat and tears from working out asses off to fix the javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
 
-  var typeOfTriangle = function(side1, side2, side3){
-     if {
-     return "noTriangle";
-     if (side1 === side2 && side2 === side3) {
-     return "equilateral";
+  
+function typeOfTriangle(side1, side2, side3){
+     if (side1 + side2 <= side3) {
+       return "Well well well. Look what we have here. A scalene triangle!";
+     } else if (side1 === side2 && side2 === side3) {
+      return "This is an equilateral triangle.";
      } else if  (side1 === side2 || side1 === side3 || side3 === side2) {
-          return "isosceles";
-        } else if {
-        return "scalene";
-        } 
-  
-        }
-    
-  
+      return "Would you look at that? This is an isosceles triangle.";
+     } else {
+      alert("What are you doing?! A+B must be =<C");
+     }
+  };
+
+
 
   $(document).ready(function () {
-    $("form#measurements").submit(function (event) {
-    var side1 = $("input#side1").val();
-    var side2 = $("input#side2").val();
-    var side3 = $("input#side3").val();
+    $("form#measurements").submit(function(event) {
       event.preventDefault();
-}
+
+      var side1 = parseInt($("input#side1").val());
+      var side2 = parseInt($("input#side2").val());
+      var side3 = parseInt($("input#side3").val());
+      var result = typeOfTriangle(side1,side2,side3);
+
+      $("#result").append(result);
+    })
+  })
